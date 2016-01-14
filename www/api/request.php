c62e7a6fdf1826a4c6ad7c383aa34eca7ddfaf74
Wietse Neven <info@wn-multimedia.nl>
2016-01-14 12:24:43 +0100

    api improvements

--- www/api/request.php
+++ www/api/request.php
@@ -61,9 +61,48 @@ elseif ($requestType == 'startups'):
 
 	echo $startups;
 	if(!$authCode || !$startups){
-
 		die;
 	}
+
+	$insertQuery = $db->prepare("
+		INSERT INTO
+			users (authCode, startups)
+		SELECT
+			?, ?
+		FROM DUAL
+		WHERE NOT EXISTS
+			(SELECT
+			 	*
+		    FROM
+		    	users
+	        WHERE
+	        	authCode=?)
+  	");
+
+	$getUserQuery = $db->prepare("
+		SELECT
+			*
+		FROM
+			users
+		WHERE
+			authCode = ?
+	");
+
+	// voeg parameters toe aan je statement
+	$insertParams = array($authCode, 0, $authCode);
+	$getUserParams = array($authCode);
+	// voer de statement met de parameters uit
+	$insertQuery->execute($insertParams);
+
+	$getUserQuery->execute($getUserParams);
+	// sla het resultaat op in een array
+	$result = $getUserQuery->fetchAll(PDO::FETCH_ASSOC);
+
+
+	// vertaal het resultaat naar een json object
+	$json = json_encode($result, JSON_PRETTY_PRINT);
+	// toon het json object
+	echo $json;
 
 
 endif;
